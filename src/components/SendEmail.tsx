2d0507d499b579937492447dc9cdbe364a6fae56
mark <markgolah69@gmail.com>
2023-10-18 18:10:13 +0100

    page update

--- src/components/SendEmail.tsx
+++ src/components/SendEmail.tsx
@@ -7,68 +7,74 @@ import { useChangePasswordMutation } from "../store/features/user/usersApiSlice"
 
 const SendEmail = () => {
 
-	const [changePassword, { isLoading }] = useChangePasswordMutation();
+    const [changePassword, { isLoading }] = useChangePasswordMutation();
 
-	const content = isLoading ? <h1>Submitting ...</h1> :
+    const content = isLoading ? <h1>Submitting ...</h1> :
 
-		<Formik
+        <Formik
 
-			initialValues={{
+            initialValues={{
 
-				oldPassword: '',
-				newPassword: '',
-				confirmPassword: '',
-			}}
+                oldPassword: '',
+                newPassword: '',
+                confirmPassword: '',
+            }}
 
 
 
-			onSubmit={
-				async values => {
-					try {
-						const data = await changePassword(values).unwrap();
-						console.log(data, 'USER PASSWORD++++++');
-						return data;
+            onSubmit={
+                async values => {
+                    try {
+                        const data = await changePassword(values).unwrap();
+                        console.log(data, 'USER PASSWORD++++++');
+                        return data;
 
-					} catch (error) {
-						console.log(error);
+                    } catch (error) {
+                        console.log(error);
 
-					}
-				}
-			}
+                    }
+                }
+            }
 
-		>
-			<div>
+        >
+            <div>
 
-				<div className='flex h-full overflow-hidden'>
-					<div className='basis-[12%] h-[100vh] '>
-						<SidebarV2 />
-					</div>
-					<div className='basis-[88%] border h-[100vh]'>
-						<NavbarDash />
-						<h1 className='header'>Send Email</h1>
+                <div className='flex h-full overflow-hidden'>
+                    <div className='basis-[12%] h-[100vh] '>
+                        <SidebarV2 />
+                    </div>
+                    <div className='basis-[88%] border h-[100vh]'>
+                        <NavbarDash />
+                        <h1 className='header'>Send Email</h1>
 
-						<div className="flex flex-col w-[80vw] m-auto p-2 rounded">
+                        <div className="flex flex-col w-[80vw] m-auto p-2 rounded">
 
-							<form className="main-container
-">
-								<div className="mb-6">
-									<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-									<input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required></input>
-								</div>
-								<div className="mb-6">
-									<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">body</label>
-									<input type="text" id="text"  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required></input>
-								</div>
-								<button type="submit" className="btn-btn">Send Email</button>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
+                            <form className="main-container">
 
-		</Formik>
 
-	return content;
+                            <div className="mb-6">
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">id</label>
+                                    <input type="text" id="id" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" name="id"></input>
+                                </div>
+
+                                <div className="mb-6">
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                                    <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" name="email"></input>
+                                </div>
+                                <div className="mb-6">
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">body</label>
+                                    <input type="text" id="text" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" name="bo"></input>
+                                </div>
+                                <button type="submit" className="btn-btn">Send Email</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </Formik>
+
+    return content;
 
 
 }
--- src/components/SendPushnotif.tsx
+++ src/components/SendPushnotif.tsx
@@ -7,73 +7,78 @@ import { useChangePasswordMutation } from "../store/features/user/usersApiSlice"
 
 const SendPushNotification = () => {
 
-	const [changePassword, { isLoading }] = useChangePasswordMutation();
+    const [changePassword, { isLoading }] = useChangePasswordMutation();
 
-	const content = isLoading ? <h1>Submitting ...</h1> :
+    const content = isLoading ? <h1>Submitting ...</h1> :
 
-		<Formik
+        <Formik
 
-			initialValues={{
+            initialValues={{
 
-				oldPassword: '',
-				newPassword: '',
-				confirmPassword: '',
-			}}
+                oldPassword: '',
+                newPassword: '',
+                confirmPassword: '',
+            }}
 
 
 
-			onSubmit={
-				async values => {
-					try {
-						const data = await changePassword(values).unwrap();
-						console.log(data, 'USER PASSWORD++++++');
-						return data;
+            onSubmit={
+                async values => {
+                    try {
+                        const data = await changePassword(values).unwrap();
+                        console.log(data, 'USER PASSWORD++++++');
+                        return data;
 
-					} catch (error) {
-						console.log(error);
+                    } catch (error) {
+                        console.log(error);
 
-					}
-				}
-			}
+                    }
+                }
+            }
 
-		>
-			<div>
+        >
+            <div>
 
-				<div className='flex h-full overflow-hidden'>
-					<div className='basis-[12%] h-[100vh] '>
-						<SidebarV2 />
-					</div>
-					<div className='basis-[88%] border h-[100vh]'>
-						<NavbarDash />
-						<h1 className='header'>Send Push Notification</h1>
+                <div className='flex h-full overflow-hidden'>
+                    <div className='basis-[12%] h-[100vh] '>
+                        <SidebarV2 />
+                    </div>
+                    <div className='basis-[88%] border h-[100vh]'>
+                        <NavbarDash />
+                        <h1 className='header'>Send Push Notification</h1>
 
-						<div className="flex flex-col w-[80vw] m-auto p-2 rounded">
+                        <div className="flex flex-col w-[80vw] m-auto p-2 rounded">
 
-							<form className="main-container
-">
-								<div className="mb-6">
-									<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-									<input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required></input>
-								</div>
-								<div className="mb-6">
-									<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">token</label>
-									<input type="text" id="text"  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required></input>
-								</div>
-								<div className="mb-6">
-									<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">description</label>
-									<input type="text" id="description" placeholder="description" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required></input>
-                                    
-								</div>
-								<button type="submit" className="btn-btn">Push Notification</button>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
+                            <form className="main-container">
 
-		</Formik>
+                                <div className="mb-6">
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">id</label>
+                                    <input type="text" id="id" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" name="Email"></input>
+                                </div>
 
-	return content;
+                                <div className="mb-6">
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                                    <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" name="Email"></input>
+                                </div>
+                                <div className="mb-6">
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">token</label>
+                                    <input type="text" id="text" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" name="token"></input>
+                                </div>
+                                <div className="mb-6">
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">description</label>
+                                    <input type="text" id="description" placeholder="description" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" name="description"></input>
+
+                                </div>
+                                <button type="submit" className="btn-btn">Push Notification</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </Formik>
+
+    return content;
 
 
 }
